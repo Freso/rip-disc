ba1cc0d3a2979286bf577c437e52fb8d1f4c356a
Frederik “Freso” S. Olesen <freso.dk@gmail.com>
2017-09-18 14:15:14 +0200

    Comment out all shell script code

--- rip-disc.py
+++ rip-disc.py
@@ -20,84 +20,84 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-TMPDIR=`mktemp -d '/tmp/rip-disc.XXX'`
-DESTDIR=`pwd`
-TAGGER=`which picard`
-RELEASE_ID=''
-DRIVE='/dev/cdrom'
+#TMPDIR=`mktemp -d '/tmp/rip-disc.XXX'`
+#DESTDIR=`pwd`
+#TAGGER=`which picard`
+#RELEASE_ID=''
+#DRIVE='/dev/cdrom'
 
-while getopts 't:d:r:c:' OPT; do
-  case $OPT in
-    t)
-      echo "Setting TMPDIR to $OPTARG."
-      TMPDIR=$OPTARG
-      ;;
-    d)
-      echo "Setting DESTDIR to $OPTARG."
-      DESTDIR=$OPTARG
-      ;;
-    r)
-      echo "Setting RELEASE_ID to $OPTARG."
-      echo "... See release on MusicBrainz: https://musicbrainz.org/release/$OPTARG"
-      RELEASE_ID=$OPTARG
-      ;;
-    c)
-      echo "Using disk drive $OPTARG."
-      DRIVE=$OPTARG
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-    :)
-      echo "Option -$OPTARG requires an argument." >&2
-      exit 1
-      ;;
-  esac
-done
+#while getopts 't:d:r:c:' OPT; do
+  #case $OPT in
+    #t)
+      #echo "Setting TMPDIR to $OPTARG."
+      #TMPDIR=$OPTARG
+      #;;
+    #d)
+      #echo "Setting DESTDIR to $OPTARG."
+      #DESTDIR=$OPTARG
+      #;;
+    #r)
+      #echo "Setting RELEASE_ID to $OPTARG."
+      #echo "... See release on MusicBrainz: https://musicbrainz.org/release/$OPTARG"
+      #RELEASE_ID=$OPTARG
+      #;;
+    #c)
+      #echo "Using disk drive $OPTARG."
+      #DRIVE=$OPTARG
+      #;;
+    #\?)
+      #echo "Invalid option: -$OPTARG" >&2
+      #exit 1
+      #;;
+    #:)
+      #echo "Option -$OPTARG requires an argument." >&2
+      #exit 1
+      #;;
+  #esac
+#done
 
-TRACK_TEMPLATE='%A - %d (%y) [%X]/%t. %a - %n'
-DISC_TEMPLATE='%A - %d (%y) [%X]/%A - %d'
+#TRACK_TEMPLATE='%A - %d (%y) [%X]/%t. %a - %n'
+#DISC_TEMPLATE='%A - %d (%y) [%X]/%A - %d'
 
-_ripcmd="whipper cd --device='$DRIVE' rip --cdr --prompt \
---working-directory='$TMPDIR' --output-directory='' \
---track-template='$TRACK_TEMPLATE' --disc-template='$DISC_TEMPLATE' \
---release-id='$RELEASE_ID'"
+#_ripcmd="whipper cd --device='$DRIVE' rip --cdr --prompt \
+#--working-directory='$TMPDIR' --output-directory='' \
+#--track-template='$TRACK_TEMPLATE' --disc-template='$DISC_TEMPLATE' \
+#--release-id='$RELEASE_ID'"
 
-echo "Ripping to: $TMPDIR"
+#echo "Ripping to: $TMPDIR"
 
-eject --trayclose "$DRIVE"
+#eject --trayclose "$DRIVE"
 
-echo '... Extracting TOC.'
-TOCFILE=$(mktemp --dry-run --suffix .toc)
-cdrdao read-toc --device "$DRIVE" "$TOCFILE"
+#echo '... Extracting TOC.'
+#TOCFILE=$(mktemp --dry-run --suffix .toc)
+#cdrdao read-toc --device "$DRIVE" "$TOCFILE"
 
-echo '... Ripping the CD.'
-echo "    Using: $_ripcmd"
+#echo '... Ripping the CD.'
+#echo "    Using: $_ripcmd"
 
-whipper cd --device="$DRIVE" rip --cdr --prompt \
---working-directory="$TMPDIR" --output-directory='' \
---track-template="$TRACK_TEMPLATE" --disc-template="$DISC_TEMPLATE" \
---release-id="$RELEASE_ID" || exit 1
+#whipper cd --device="$DRIVE" rip --cdr --prompt \
+#--working-directory="$TMPDIR" --output-directory='' \
+#--track-template="$TRACK_TEMPLATE" --disc-template="$DISC_TEMPLATE" \
+#--release-id="$RELEASE_ID" || exit 1
 
-discdir=`ls $TMPDIR/`
-echo "... Working with $discdir."
+#discdir=`ls $TMPDIR/`
+#echo "... Working with $discdir."
 
-echo '... Recompressing and calculating ReplayGain.'
-flac --preserve-modtime --replay-gain --best --verify --force $TMPDIR/*/*.flac
+#echo '... Recompressing and calculating ReplayGain.'
+#flac --preserve-modtime --replay-gain --best --verify --force $TMPDIR/*/*.flac
 
-echo '... Additionally tag files.'
-$TAGGER "$TMPDIR/$discdir"
+#echo '... Additionally tag files.'
+#$TAGGER "$TMPDIR/$discdir"
 
-echo '... Submitting to AcousticBrainz.'
-find "$TMPDIR/$discdir" -name '*.flac' -print0 | parallel --null --eta abzsubmit
+#echo '... Submitting to AcousticBrainz.'
+#find "$TMPDIR/$discdir" -name '*.flac' -print0 | parallel --null --eta abzsubmit
 
-# echo '... Generating torrent file.'
-# mktor ...
+## echo '... Generating torrent file.'
+## mktor ...
 
-echo '... Compress into archive.'
-mv "$TOCFILE" "$TMPDIR/$discdir/CD.toc"
-7z a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on -mmt=on "$TMPDIR/$discdir.7z" "$TMPDIR/$discdir"
+#echo '... Compress into archive.'
+#mv "$TOCFILE" "$TMPDIR/$discdir/CD.toc"
+#7z a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on -mmt=on "$TMPDIR/$discdir.7z" "$TMPDIR/$discdir"
 
-echo '... Moving archive.'
-rsync -avz --remove-sent-files --progress "$TMPDIR/$discdir.7z" "$DESTDIR" && rm -rf "$TMPDIR/$discdir"
+#echo '... Moving archive.'
+#rsync -avz --remove-sent-files --progress "$TMPDIR/$discdir.7z" "$DESTDIR" && rm -rf "$TMPDIR/$discdir"
